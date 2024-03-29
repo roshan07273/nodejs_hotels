6a7c987aa20f99a92798f1003acd48a53879e5d2
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2024-03-29 09:39:27 +0530

    Update learning.js

--- learning.js
+++ learning.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+const mongoURL = process.env.mongoURL || 3000;
 app.get("/", function (req, res) {
   res.send("Welcome to my Hotel!");
 });
@@ -13,6 +15,7 @@ const menuRoutes = require("./routes/menuRoutes");
 
 app.use("/menu", menuRoutes);
 app.use("/person", personRoutes);
+
 app.listen(3000, () => {
   console.log("Roshan Raturi is listening on 3000");
 });
